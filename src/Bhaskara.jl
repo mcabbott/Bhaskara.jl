cd364b181ebe08408f77326783d9b2b3edec9266
Michael Abbott <improbable@gmail.com>
2019-03-17 18:39:27 +0100

    003

--- src/Bhaskara.jl
+++ src/Bhaskara.jl
@@ -1,25 +1,25 @@
 module Bhaskara
 
-export sinπ, sin2π, cosπ, sind180
+export bsin, b2sin, bcos
 
 
-sinπ(θ) = 16(π-θ)θ / (5π^2 - 4(π-θ)θ)
+bsin(θ) = 16(π-θ)θ / (5π^2 - 4(π-θ)θ)
 
-sin2π(θ) = sign(θ) * sinπ(abs(θ))
+b2sin(θ) = sign(θ) * bsin(abs(θ))
 
-sin(θ::T) where T<:Number = sin2π(mod(θ+π, T(2)*π) - π)
+sin(θ::T) where T<:Number = b2sin(mod(θ+π, T(2)*π) - π)
 
 
 sind180(x) = 4(180-x)x / (40500 - (180-x)x)
 
-sind360(x) = sign(x) * sin180(abs(x))
+sind360(x) = sign(x) * sind180(abs(x))
 
 sind(x::Number) = sind360(mod(x+180,360)-180)
 
 
-cosπ(θ) = (π^2 - 4θ^2) / (π^2+θ^2)
+bcos(θ) = (π^2 - 4θ^2) / (π^2+θ^2)
 
-cos(θ::T) where T<:Number = sin2π(mod(θ+π/T(2), T(2)*π) - π)
+cos(θ::T) where T<:Number = b2sin(mod(θ-π/T(2), T(2)*π) - π)
 
 
 tan(θ::Number) = sin(θ) / cos(θ)
